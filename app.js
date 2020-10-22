5690c8968bdf58e9a5276102692fa67225f05280
Victorowsky <Victorowskyy@gmail.com>
2020-10-22 09:34:05 +0200

    admin login, admin news

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var config = require("./config");
+var mongoose = require("mongoose");
+
+mongoose.connect(config.db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
 
 var indexRouter = require("./routes/index");
 var newsRouter = require("./routes/news");
 var quizRouter = require("./routes/quiz");
 var adminRouter = require("./routes/admin");
-
 var app = express();
 
 // view engine setup
--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  db:
+    "mongodb+srv://admin:nKmAxGbmAtQHXYYY@cluster0.ktmcd.mongodb.net/express?retryWrites=true&w=majority",
   keySession: ["TWOJKLUCZ"],
   maxAgeSession: 24 * 60 * 60 * 1000,
 };
